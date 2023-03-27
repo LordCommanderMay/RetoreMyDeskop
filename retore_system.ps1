2660c9783e49d82b3c0092ea0dacf9dd7c50a269
Sean May <40738896+LordCommanderMay@users.noreply.github.com>
2023-03-27 02:00:34 -0400

    Update retore_system.ps1
    added install winget func and comments

--- retore_system.ps1
+++ retore_system.ps1
@@ -1,12 +1,24 @@
-$A = Read-Host -Prompt "Ready to restore your system? "
-
-if ($A -eq "y" -or $A -eq "yes"){
-    Write-Output "Installing Apps!"
-    $Apps2download = Get-Content $PSScriptRoot"/App2download.txt" 
-    foreach ($app in $Apps2download) {
-         winget install -e -h --id $app
-        
-
-         
+function Install-Winget {
+    try {
+        # Check if Winget is already installed
+        winget --version | Out-Null
+    } catch {
+        # Winget is not installed, so install it using PowerShell
+        Set-ExecutionPolicy RemoteSigned -Scope CurrentUser; iex ((New-Object System.Net.WebClient).DownloadString('https://winget.ms/install.ps1'))
     }
 }
+
+# Install Winget if it's not already installed
+Install-Winget
+
+# Read the list of programs from a text file
+$programList = Get-Content -Path "programList.txt"
+
+# Loop through the list of programs and install each one using Winget with the --silent option
+foreach ($program in $programList) {
+    Write-Host "Installing $program ..."
+    winget install $program --silent
+}
+
+# Display a message when all programs have been installed
+Write-Host "All programs have been installed."
